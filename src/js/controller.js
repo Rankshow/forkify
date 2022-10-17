eb3510bd831cf20b36233aeabcfc4a211799f8d2
rankshow <uwaomaobinna20@gmail.com>
2022-10-17 23:23:28 +0100

    new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -128,6 +128,9 @@ setTimeout(function(){
   addRecipeView.renderError(err.message);
 }
 };
+const newFeature = function () {
+  console.log('welcome to the application')
+}
 
 const init = function (){
   bookmarksView.addHandlerRender(controlBookmarks)
@@ -137,7 +140,6 @@ const init = function (){
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addhandlerUplaod(controlAddRecipe);
-  console.log('welcome')
- 
+ newFeature();
 };
 init();
